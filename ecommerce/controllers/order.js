5dceb86a8e24f7a4bfa876d050bc9f2ea9145887
Chukwuemeka Mbaebie <chukwuemekambaebie@gmail.com>
2020-05-31 06:02:18 +0100

    Updates

--- ecommerce/controllers/order.js
+++ ecommerce/controllers/order.js
@@ -33,8 +33,8 @@ exports.create = (req, res) => {
         // order.products.length
         // order.amount
         const emailData = {
-            to: 'support@imeafia.com',
-            from: 'noreply@imeafia.com',
+            to: 'kaloraat@gmail.com',
+            from: 'noreply@ecommerce.com',
             subject: `A new order is received`,
             html: `
             <p>Customer name:</p>
